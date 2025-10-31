b329e3f908787f99c3263cac3d82bccb45cf2824
SommerFi <Niklas@Messerschmidt.biz>
2025-10-31 08:33:06 +0100

    YOLO Bilderkenner

---
+++ tag_04/objekt_erkenner.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+model = YOLO('yolo11n.pt')
+bild = input()
+results = model(bild)
+
+for result in results:
+    boxes = result.boxes       
+    names = result.names        
+    confidence = boxes.conf     
+
+for box in boxes:
+    x1, y1, x2, y2 = box.xyxy[0]  # Koordinaten der Box
+    confidence = box.conf[0]       # Wie sicher? (0.0 bis 1.0)
+    class_id = box.cls[0]          # Was wurde erkannt?
+    print(f"Gefunden: {names[int(class_id)]} mit {confidence:.2f} Sicherheit")
+
+results[0].show()
